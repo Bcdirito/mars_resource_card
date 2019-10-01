db731d39564b40a0c9b89978ceb2754889ae353f
Bcdirito <bdirito91@gmail.com>
2019-10-01 12:33:52 -0400

    minor style changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,7 @@ import "../css/home.css"
 
 const Home = (props) => (
     <div className="welcomeText">
-        <h1>
-            Welcome to your Terraforming Mars Resource Card
-        </h1>
+        <h1>Welcome to your Terraforming Mars Resource Card</h1>
         <Link to={localStorage["color"] ? "/continue-game" : "/create-player"} className="enterLink"><h2>Enter</h2></Link>
     </div>
 )
