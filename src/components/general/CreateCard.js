76b306545430c6e59a58d7c8c68459d2cf108fc4
Bcdirito <bdirito91@gmail.com>
2019-12-31 14:14:32 -0500

    added preview functionality

--- src/components/general/CreateCard.js
+++ src/components/general/CreateCard.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react'
 
-export default class CreateCard extends Component {
-    state = {
-        corporateCredits: 0,
-        cardsBought: 0
-    }
+const CreateCard = (props) => {
+    return (
+        <div>
+            <h1>Set Up Phase</h1>
+            <form onSubmit={(e) => props.handleSubmit(e)}>
+                <label>Total Starting Credits:</label>
+                <input type="text" name="corporateCredits" value={props.creditData.corporateCredits} onChange={(e) => props.handleCreditChange(e)}/>
+                <label>How Many Cards Are You Buying?</label>
+                <input type="number" name="cardsBought"  value={props.creditData.cardsBought} onChange={(e) => props.handleCreditChange(e)} />
 
-    handleChange = (e) => {
-        e.preventDefault()
-        this.setState({
-            ...this.state,
-            [e.target.name]: Number(e.target.value)
-        }, () => console.log(this.state))
-    }
-
-    calculateStartingCredits = () => {
-        return this.state.corporateCredits - (3*this.state.cardsBought)
-    }
-
-    handleSubmit = (e) => {
-
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Set Up Phase</h1>
-                <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <label>Total Starting Credits:</label>
-                    <input type="text" name="corporateCredits" value={this.state.corporateCredits} onChange={(e) => this.handleChange(e)}/>
-                    <label>How Many Cards Are You Buying?</label>
-                    <input type="number" name="cardsBought"  value={this.state.cardsBought} onChange={(e) => this.handleChange(e)} />
-
-                    <label>Starting Credits:</label><span>{this.calculateStartingCredits()}</span>
-                </form>
-            </div>
-        )
-    }
+                <label>Starting Credits:</label><span>{props.creditData.startingCredits}</span>
+                <input type="submit" value="Start Game" />
+            </form>
+        </div>
+    )
 }
+
+export default CreateCard
--- src/components/general/PlayerCreationContainer.js
+++ src/components/general/PlayerCreationContainer.js
@@ -3,6 +3,7 @@ import PlayerCreationComponent from "./PlayerCreationComponent"
 import CreateCard from "./CreateCard"
 import {connect} from "react-redux"
 import {createPlayer} from "../../store/actions/playerCreationActions"
+import {changeResources} from "../../store/actions/resourceActions"
 
 class PlayerCreationContainer extends Component {
     state = {
@@ -13,6 +14,11 @@ class PlayerCreationContainer extends Component {
             color: "red",
             formName: "redPlayer",
             solo: false
+        },
+        creditData: {
+            corporateCredits: 0,
+            cardsBought: 0,
+            startingCredits: 0
         }
     }
 
@@ -49,28 +55,66 @@ class PlayerCreationContainer extends Component {
         })
     }
 
+    handleCreditChange = (e) => {
+        e.preventDefault()
+        let targetName = e.target.name
+        this.setState({
+            ...this.state,
+            creditData: {
+                ...this.state.creditData,
+                [e.target.name]: Number(e.target.value)
+            }
+        }, () => this.calculateStartingCredits(targetName))
+    }
+
+    calculateStartingCredits = (name) => {
+        if (name === "corporateCredits") {
+            this.setState({
+                ...this.state,
+                creditData: {
+                    ...this.state.creditData,
+                    startingCredits: this.state.creditData.corporateCredits
+                }
+            })
+        } else {
+            this.setState({
+                ...this.state,
+                creditData: {
+                    ...this.state.creditData,
+                    startingCredits: this.state.creditData.corporateCredits - (3*this.state.creditData.cardsBought)
+
+                }
+            })
+        }
+    }
+
     handleSubmit = e => {
         e.preventDefault()
-        if (this.state.color !== "" && this.state.username !== ""){
-            this.props.createPlayer(e.target.username.value, e.target.color.value, e.target.solo.value)
+        if (this.state.creditData.corporateCredits !== 0){
+            this.props.createPlayer(this.state.playerData.username, this.state.playerData.color, this.state.playerData.solo)
+            this.props.changeResources("credits", this.state.creditData.startingCredits)
             this.props.history.replace("/card")
         } else {
-            alert("Please enter your name")
+            alert("Please enter your corporations starting credits")
         }
     }
 
     renderCreateCard = e => {
         e.preventDefault()
-        this.setState({
-            ...this.state,
-            playerCreation: false
-        })
+        if (this.state.playerData.color !== "" && this.state.playerData.username !== "") {
+            this.setState({
+                ...this.state,
+                playerCreation: false
+            })
+        } else {
+            alert("Please enter your name")
+        }
     }
 
     render() {
         return (
             <div>
-                {this.state.playerCreation === true ? <PlayerCreationComponent playerData={this.state.playerData} handlePlayerCreation={(e) => this.handlePlayerCreation(e)} renderCreateCard={e => this.renderCreateCard(e)} handleColorChange={e => this.handleColorChange(e)}/> : <CreateCard handleSubmit={(e) => this.handleSubmit(e)}/>}
+                {this.state.playerCreation === true ? <PlayerCreationComponent playerData={this.state.playerData} handlePlayerCreation={(e) => this.handlePlayerCreation(e)} renderCreateCard={e => this.renderCreateCard(e)} handleColorChange={e => this.handleColorChange(e)}/> : <CreateCard handleCreditChange={(e) => this.handleCreditChange(e)} handleSubmit={(e) => this.handleSubmit(e)} creditData={this.state.creditData}/>}
             </div>
         )
     }
@@ -79,6 +123,7 @@ class PlayerCreationContainer extends Component {
 const mapDispatchToProps = (dispatch) => {
     return {
         createPlayer: (playerName, color, gameType) => dispatch(createPlayer(playerName, color, gameType)),
+        changeResources: (resource, amt) => dispatch(changeResources(resource, amt)),
         logout: () => dispatch({type: "LOGOUT_PLAYER"}),
         clearResources: () => dispatch({type: "CLEAR_RESOURCES"})
     }
