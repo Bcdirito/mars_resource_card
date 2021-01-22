76295db20f78d618cceeb90dd0dd01d7598afdfd
Brian DiRito <bdirito91@gmail.com>
2021-01-21 21:17:40 -0500

    revised mobile modal

--- src/utils/mobileChecker.js
+++ src/utils/mobileChecker.js
@@ -1,49 +1,10 @@
-const mobileChecker = (nav) => {
-    let userAgent = nav.userAgent
-
-    let windowObj = {
-      iPhone: {
-        cb: userAgent.match(/iPhone/i),
-        device: "mobile"
-      },
-      iPad: {
-        cb: userAgent.match(/iPad/i),
-        device: "tablet"
-      },
-      iPod: {
-        cb: userAgent.match(/iPod/i),
-        device: "mobile"
-      },
-      android: {
-        cb: userAgent.match(/Android/i),
-        device: "mobile"
-      },
-      webOs: {
-        cb: userAgent.match(/webOS/i),
-        device: "mobile"
-      },
-      blackBerry: {
-        cb: userAgent.match(/BlackBerry/i),
-        device: "mobile"
-      },
-      windowsPhone: {
-        cb: userAgent.match(/Windows Phone/i),
-        device: "mobile"
-      }
-    }
-
+const mobileChecker = () => {
     localStorage.removeItem("device")
+    let device = window.innerWidth >= 430 ? "Desktop" : "Mobile"
+    device = window.innerWidth > window.innerHeight && window.innerHeight <= 430 ? "Mobile" : "Desktop"
 
-    for (const key in windowObj) {
-      if (windowObj[key]["cb"] !== null) {
-        console.log(key)
-        localStorage.setItem("device", windowObj[key]["device"])
-        return localStorage.getItem("device")
-      }
-    }
-
-    localStorage.setItem("device", "desktop")
-    return localStorage.getItem("device")
+    localStorage.setItem("device", device)
+    return device
 }
 
 module.exports = mobileChecker
